0f0053d967d4e764e874398c2083b8d99209dc28
Ram Krishna Sharma <ram.krishna.sharma@cern.ch>
2022-02-22 00:47:40 +0100

    Redirected all outputs to the directory xs_125.0

--- RunEverything.py
+++ RunEverything.py
@@ -4,9 +4,20 @@ import argparse
 from python.collectInputs import collect
 from python.obsList import obsNamesBinsDict
 
+
+# Kept for record of models (copied from runHZZFiducialXS.py)
+# modelNames = "SM_125,SMup_125,SMdn_125" #,'VBF_powheg_JHUgen_125']
+# # do all models
+# #if (not 'jet' in obsName):
+# #    modelNames = ['SM_125','ggH_powheg_JHUgen_125', 'VBF_powheg_JHUgen_125', 'WH_powheg_JHUgen_125', 'ZH_powheg_JHUgen_125', 'ttH_powheg_JHUgen_125']
+# #else:
+# #    modelNames = ['SM_125','ggH_powheg_JHUgen_125', 'VBF_powheg_JHUgen_125', 'WH_powheg_JHUgen_125', 'ZH_powheg_JHUgen_125']
+
 parser = argparse.ArgumentParser(description='Input arguments')
 parser.add_argument( '-s', dest='step', default=1, choices=[1, 2, 3, 4, 5], type=int, help='Which step to run')
 parser.add_argument( '-c', dest='channels', nargs="+",  default=["4mu", "4e", "2e2mu", "4l"], help='list of channels')
+parser.add_argument( '-model', dest='modelNames', default="SM_125,SMup_125,SMdn_125",
+                        help='Names of models for unfolding, separated by , (comma) . Default is "SM_125"')
 parser.add_argument( '-p', dest='NtupleDir', default="/eos/home-v/vmilosev/Skim_2018_HZZ/WoW/", help='Path of ntuples')
 parser.add_argument( '-m', dest='HiggsMass', default=125.0, type=float, help='Higgs mass')
 parser.add_argument( '-r', dest='RunCommand', default=0, type=int, choices=[0, 1], help="if 1 then it will run the commands else it will just print the commands")
@@ -62,8 +73,9 @@ for obsName, obsBin in obsNamesBinsDict.items():
         print("="*51)
         print("Running Background template maker")
         print("="*51)
-        command = 'python -u runHZZFiducialXS.py --dir="{NtupleDir}" --obsName="{obsName}" --obsBins="{obsBins}" --redoTemplates --templatesOnly'.format(
-                obsName = obsName, obsBins = obsBin, NtupleDir = args.NtupleDir
+        # FIXME: Check if we need modelNames in step-4 or not
+        command = 'python -u runHZZFiducialXS.py --dir="{NtupleDir}" --obsName="{obsName}" --obsBins="{obsBins}" --modelNames {modelNames} --redoTemplates --templatesOnly '.format(
+                obsName = obsName, obsBins = obsBin, NtupleDir = args.NtupleDir, modelNames= args.modelNames
         )
         print("Command: {}".format(command))
         if (args.RunCommand): os.system(command)
@@ -78,8 +90,8 @@ for obsName, obsBin in obsNamesBinsDict.items():
         os.system(copyCommand)
 
         # command = 'nohup python -u runHZZFiducialXS.py --obsName="{obsName}" --obsBins="{obsBins}"  --calcSys --asimovMass {HiggsMass}  >& log/log_{obsName}_Run2Fid.txt &'.format(
-        command = 'python -u runHZZFiducialXS.py --obsName="{obsName}" --obsBins="{obsBins}"  --calcSys --asimovMass {HiggsMass}'.format(
-                obsName = obsName, obsBins = obsBin, HiggsMass = args.HiggsMass
+        command = 'python -u runHZZFiducialXS.py --obsName="{obsName}" --obsBins="{obsBins}"  --calcSys --asimovMass {HiggsMass} --modelNames {modelNames}'.format(
+                obsName = obsName, obsBins = obsBin, HiggsMass = args.HiggsMass, modelNames= args.modelNames
         )
         print("Command: {}".format(command))
         if (args.RunCommand): os.system(command)
